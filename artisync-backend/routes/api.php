9a931f4d01575004b67c6d39383566b4b977bb25
Afaf <afaf20ahadad@gmail.com>
2026-05-29 12:33:31 +0000

    feat: implement maintenance history retrieval

--- artisync-backend/routes/api.php
+++ artisync-backend/routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/machines/{machine}/maintenances', [App\Http\Controllers\MaintenanceController::class, 'index']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('machines', MachineController::class);
     Route::post('/machines/{machine}/maintenances', [App\Http\Controllers\MaintenanceController::class, 'store']);
